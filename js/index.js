8b1fda9f4ef3b1ffb50959410ff4c55cf9fe1107
Ilya_pinchuk <deffokorn71@gmail.com>
2021-05-07 23:30:43 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let countOfLayer = +prompt('Введите количество  строк пирамиды:');
+
+let countOfSpace = countOfLayer -1  ;
+let string = '';
+let stringSpaces = '';
+let strStar = '*';
+
+for (let i = 0 ; i < countOfLayer; i++ ){
+    stringSpaces = stringSpaces.concat(' ');
+}
+
+for (let i = 0 ; i < countOfLayer ; i++){
+    string = string.concat(stringSpaces , strStar ,  '\n');
+    countOfSpace = countOfSpace - 1;
+    stringSpaces = stringSpaces.substr(0 , countOfSpace);
+    strStar = strStar.concat('**');
+}
+alert(string)
+console.log(string)
